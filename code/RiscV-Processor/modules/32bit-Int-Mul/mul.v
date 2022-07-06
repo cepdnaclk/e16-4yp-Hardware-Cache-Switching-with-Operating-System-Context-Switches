f965b4281e5af6cbc08e3107fb62ae6d7b9e3f41
wishva <wishvasd1234@gmail.com>
2022-07-06 12:29:20 +0530

    added multiplication fun

--- code/RiscV-Processor/modules/32bit-Int-Mul/mul.v
+++ code/RiscV-Processor/modules/32bit-Int-Mul/mul.v
@@ -1,33 +1,32 @@
-module mul(RESULT, DATA1, DATA2, SELECT, CLK);
+module mul(RESULT, DATA1, DATA2, SELECT);
 
   input [31:0] DATA1, DATA2;
   input [2:0] SELECT;
-  input wire CLK;
+  // input wire CLK;
   // output [63:0] RESULTM;
-  output [31:0] RESULT;
+  output reg [31:0] RESULT;
 
-  wire [31:0] MUL,MULH,MULHU,MULHSU,DIV,DIVU,REM,REMU;
-
-
+  wire [63:0] MUL,MULH,MULHU,MULHSU;
+  wire [31:0] DIV,DIVU,REM,REMU;
   //multiplication 
-	assign MUL = DATA1 * DATA2;                      // Multiplication
-  assign MULH = DATA1 * DATA2;                        // Multiplication (Signed)
+	assign MUL = $signed(DATA1) * $signed(DATA2);                      // Multiplication
+  // assign MULH = (DATA1 * DATA2)[63:32];                      // Multiplication (Signed)
   assign MULHU = $unsigned(DATA1) * $unsigned(DATA2);    // Multiplication (Unsigned)
   assign MULHSU = $signed(DATA1) * $unsigned(DATA2);     // Multiplication (Signed x UnSigned)
   
   //and division instructions
-	assign DIV = DATA1 / DATA2;                           // Division
+	assign DIV = $signed(DATA1) / $signed(DATA2);                           // Division
   assign DIVU = $unsigned(DATA1) / $unsigned(DATA2);    // Division Unsigned
-  assign REM = DATA1 % DATA2;                           // Remainder
+  assign REM = $signed(DATA1) % $signed(DATA2);                           // Remainder
   assign REMU = DATA1 % DATA2;                          // Remainder Unsigned
 
   always @(*) 
   begin
     case (SELECT)
-      3'd0: RESULT = MUL;
-      3'd1: RESULT = MULH;
-      3'd2: RESULT = MULHU;
-      3'd3: RESULT = MULHSU;
+      3'd0: RESULT = MUL[31:0]; //mul
+      3'd1: RESULT = MUL[63:32];  //mulh
+      3'd2: RESULT = MULHU[63:32]; //mulhu
+      3'd3: RESULT = MULHSU[63:32];  //mulhsu
 
       3'd4: RESULT = DIV;
       3'd5: RESULT = DIVU;
@@ -36,6 +35,5 @@ module mul(RESULT, DATA1, DATA2, SELECT, CLK);
     endcase
 
   end
-
 
 endmodule
