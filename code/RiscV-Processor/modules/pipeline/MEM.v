2da14fd0946a585fae6be0ff90189950a93a6001
wishva <wishvasd1234@gmail.com>
2022-07-12 14:57:24 +0530

    added mem pipeline reg

---
+++ code/RiscV-Processor/modules/pipeline/MEM.v
@@ -0,0 +1,26 @@
+module MEM(
+  mem_out,
+  write_reg_en,
+  write_address,
+  write_data,
+  reset,
+  clk
+  );
+
+  input [31:0] write_data;
+  input write_reg_en, reset, clk;
+  input [4:0] write_address;
+  output [37:0] mem_out;
+
+  always @(posedge clk)
+  begin
+
+    if(reset)begin
+      mem_out <= 37'd0;
+    end else begin
+      mem_out <= {write_reg_en, write_address, write_data};
+    end
+
+  end
+
+endmodule
