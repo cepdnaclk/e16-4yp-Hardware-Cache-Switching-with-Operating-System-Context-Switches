f7ba090a1065c208689bbbc1e8032915a70d857d
wishva <wishvasd1234@gmail.com>
2022-07-13 08:55:03 +0530

    added pipeline upto td

--- code/RiscV-Processor/modules/pipeline/ID.v
+++ code/RiscV-Processor/modules/pipeline/ID.v
@@ -17,24 +17,25 @@ module IF(
   data_2,
   mux_1_out,
   pc,
+  pc_4,
   reset,
   clk
   );
 
-  input [31:0] pc, data_1, data_2, mux_1_out;
+  input [31:0] pc_4, pc, data_1, data_2, mux_1_out;
   input mux_complmnt, mux_inp_2, mux_inp_1, mux_d_mem, write_reg_en, reset, clk, d_mem_r, d_mem_w, branch, jump;
   input [2:0] alu_op, fun_3;
   input [1:0] mux_result;
   input [4:0] write_address;
-  output [149:0] id_out;
+  output [181:0] id_out;
 
   always @(posedge clk)
   begin
 
     if(reset)begin
-      id_out <= 149'd0;
+      id_out <= 181'd0;
     end else begin
-      id_out <= {pc, mux_complmnt, mux_inp_2, mux_inp_1, mux_d_mem, write_reg_en, write_address, d_mem_r, d_mem_w, branch, jump, alu_op, fun_3, mux_result, data_1, data_2, mux_1_out};
+      id_out <= {pc_4, pc, mux_complmnt, mux_inp_2, mux_inp_1, mux_d_mem, write_reg_en, write_address, d_mem_r, d_mem_w, branch, jump, alu_op, fun_3, mux_result, data_1, data_2, mux_1_out};
     end
 
   end
