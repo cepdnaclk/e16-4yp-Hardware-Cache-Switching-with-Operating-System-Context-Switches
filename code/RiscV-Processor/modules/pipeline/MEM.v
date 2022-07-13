6076854b8d965420f2330a05a874013295708f05
wishva <wishvasd1234@gmail.com>
2022-07-13 12:38:40 +0530

    added pipeline register connctions

--- code/RiscV-Processor/modules/pipeline/MEM.v
+++ code/RiscV-Processor/modules/pipeline/MEM.v
@@ -1,6 +1,5 @@
 module MEM(
   mem_out,
-  mem_busywait,
   write_reg_en,
   write_address,
   write_data,
@@ -9,17 +8,17 @@ module MEM(
   );
 
   input [31:0] write_data;
-  input write_reg_en, reset, clk, write_address;
+  input write_reg_en, reset, clk;
   input [4:0] write_address;
-  output [38:0] mem_out;
+  output [37:0] mem_out;
 
   always @(posedge clk)
   begin
 
     if(reset)begin
-      mem_out <= 38'd0;
+      mem_out <= 37'd0;
     end else begin
-      mem_out <= {write_reg_en, write_address, write_address, write_data};
+      mem_out <= {write_reg_en, write_address, write_data};
     end
 
   end
