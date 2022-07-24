f6be08353ce24f01fa0ba47627294749a682826a
Wishva Madushanka <65881954+wishvamadushanka@users.noreply.github.com>
2022-07-24 14:52:45 +0530

    Update imem_for_icache.v

--- code/RiscV-Processor/modules/i-cache/imem_for_icache.v
+++ code/RiscV-Processor/modules/i-cache/imem_for_icache.v
@@ -19,7 +19,7 @@ reg readaccess;
 
 //Declare memory array 1024x8-bits 
 reg [7:0] memory_array [1023:0];
-
+reg [4:0] count;
 //Initialize instruction memory
 initial
 begin
@@ -91,14 +91,31 @@ end
 
 
 //Detecting an incoming memory access
-
-/*
-always @(read)
+always @ (posedge clock)
 begin
-    busywait = (read)? 1 : 0;
-    readaccess = (read)? 1 : 0;
+	if(read) begin
+		readaccess = 1'b1;
+	end else begin
+		readaccess = 1'b0;
+	end
+	
 end
-*/
+//
+
+
+
+always @(posedge clock, negedge busywait, posedge readaccess)
+begin
+	if(~busywait) begin
+		count = 5'd15;
+	end else if(readaccess) begin
+		count = count - 5'd1;
+	end
+	
+   // busywait = (read)? 1 : 0;
+    //readaccess = (read)? 1 : 0;
+end
+
 
 //Reading
 always @(posedge clock,posedge reset)
@@ -109,23 +126,14 @@ begin
     else if(read)
     begin
         busywait =1;
-        readdata[7:0]     = #40 memory_array[{address,4'b0000}];
-        readdata[15:8]    = #40 memory_array[{address,4'b0001}];
-        readdata[23:16]   = #40 memory_array[{address,4'b0010}];
-        readdata[31:24]   = #40 memory_array[{address,4'b0011}];
-        readdata[39:32]   = #40 memory_array[{address,4'b0100}];
-        readdata[47:40]   = #40 memory_array[{address,4'b0101}];
-        readdata[55:48]   = #40 memory_array[{address,4'b0110}];
-        readdata[63:56]   = #40 memory_array[{address,4'b0111}];
-        readdata[71:64]   = #40 memory_array[{address,4'b1000}];
-        readdata[79:72]   = #40 memory_array[{address,4'b1001}];
-        readdata[87:80]   = #40 memory_array[{address,4'b1010}];
-        readdata[95:88]   = #40 memory_array[{address,4'b1011}];
-        readdata[103:96]  = #40 memory_array[{address,4'b1100}];
-        readdata[111:104] = #40 memory_array[{address,4'b1101}];
-        readdata[119:112] = #40 memory_array[{address,4'b1110}];
-        readdata[127:120] = #40 memory_array[{address,4'b1111}];
-        busywait = 0;
+        readdata     =  memory_array[{address,count}];
+		  
+		  if(count == 5'd0)begin
+				busywait = 0;
+		  end else begin
+				readdata = readdata << 8;
+			end
+		  
     end
 end
  
