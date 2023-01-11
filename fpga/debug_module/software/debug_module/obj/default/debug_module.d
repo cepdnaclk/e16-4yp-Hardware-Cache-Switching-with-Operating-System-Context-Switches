7f50d7a2669c05b4836f8ecb4938876657a84dc1
wishva <wishvasd1234@gmail.com>
2023-01-11 20:11:20 +0530

    added debug module

---
+++ fpga/debug_module/software/debug_module/obj/default/debug_module.d
@@ -0,0 +1,7 @@
+obj/default/debug_module.o: debug_module.c \
+  ../debug_module_bsp//HAL/inc/io.h \
+  ../debug_module_bsp//HAL/inc/alt_types.h
+
+../debug_module_bsp//HAL/inc/io.h:
+
+../debug_module_bsp//HAL/inc/alt_types.h:
